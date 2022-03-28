5f94f57758ff5f992db697e03ed063f0474b4a3d
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-28 18:50:26 +0200

    event inputs

--- components/events/NewEventForm.tsx
+++ components/events/NewEventForm.tsx
@@ -1,66 +1,162 @@
-import { Box, Center, FormControl, Input, Slider } from 'native-base';
-import React, { useState } from 'react';
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Select,
+  Slider,
+  TextArea,
+  VStack,
+} from 'native-base';
+import React, { isValidElement, useState } from 'react';
 import { AccessibilityInfo } from 'react-native';
 
+type eventDataType = {
+  name: string | undefined;
+  price: number | undefined;
+  type: string | undefined;
+  language: string | undefined;
+  duration: number | undefined;
+  description: string | undefined;
+};
+
 function NewEventForm() {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<eventDataType>({
     name: undefined,
     price: undefined,
     type: undefined,
-    genres: undefined,
-    image: undefined,
-    poster: undefined,
+    //genres: undefined,
+    //image: undefined,
+    //poster: undefined,
     language: undefined,
     duration: undefined,
-    description: undefined,
-    external_url: undefined,
-    shows: undefined,
+    //description: undefined,
+    //external_url: undefined,
+    //shows: undefined,
   });
 
+  // const handleInputChange = (event: React.ChangeEvent) => {
+  //   const target = event.target;
+  //   // TODO: handle different target types
+  //   const value = target.value;
+  //   const name = target.name;
+  //   setFormData({ [name]: value });
+  // };
+
+  const handleSubmit = () => {
+    if (isValid()) {
+      console.log(formData);
+    } else console.log('Invalid data');
+  };
+
+  const isValid = () => {
+    // TODO: implement form validations
+    return true;
+  };
+
   return (
-    <Center w={'75%'} h="full">
-      <FormControl isRequired>
-        <FormControl.Label>Event name</FormControl.Label>
+    <Center w="full" h="full">
+      <FormControl isRequired w={'75%'} mb="2">
+        <FormControl.Label>Name</FormControl.Label>
         <Input
+          isFullWidth
+          size="md"
           type="text"
-          placeholder="Event name"
+          placeholder="Name"
           autoCapitalize="words"
           autoFocus
           value={formData.name}
-          onChangeText={(text) => setFormData({ ...formData })}
+          onChangeText={(name) => setFormData({ ...formData, name })}
         />
-        <FormControl.ErrorMessage>
-          Event name is required.
-        </FormControl.ErrorMessage>
       </FormControl>
-      <FormControl isRequired>
-        <FormControl.Label>Event description</FormControl.Label>
-        <Input
-          isRequired
-          type="text"
-          placeholder="Provide a brief description of your event"
-          autoCapitalize="sentences"
-          autoFocus
-        />
-        <FormControl.ErrorMessage>
-          Event description is required.
-        </FormControl.ErrorMessage>
-        <Slider
-          minValue={0}
-          maxValue={50}
-          step={1}
-          accessibilityLabel="Price selector slider"
-          defaultValue={20}
-          size="md"
+      <FormControl isRequired w={'75%'} mb="2">
+        <FormControl.Label>Event type</FormControl.Label>
+        <Select
+          placeholder="Select type of event"
+          onValueChange={(type) => setFormData({ ...formData, type })}
         >
-          <FormControl.Label>Price per ticket</FormControl.Label>
-
-          <Slider.Track>
-            <Slider.FilledTrack />
-          </Slider.Track>
-          <Slider.Thumb mb={-7} />
-        </Slider>
+          <Select.Item label="Theater" value="THEATER" />
+          <Select.Item label="Concert" value="CONCERT" />
+          <Select.Item label="Cinema" value="CINEMA" />
+          <Select.Item label="Circus" value="CIRCUS" />
+        </Select>
       </FormControl>
+      <FormControl isRequired w={'75%'} mb="2">
+        <HStack justifyContent={'space-between'} mb="2">
+          <VStack>
+            <FormControl.Label>Price</FormControl.Label>
+            <InputGroup>
+              <Input
+                textAlign={'right'}
+                onChangeText={(price: string) =>
+                  setFormData({ ...formData, price: +price })
+                }
+              />
+              <InputRightAddon>€</InputRightAddon>
+            </InputGroup>
+          </VStack>
+
+          <VStack>
+            <FormControl.Label>Language</FormControl.Label>
+            <Select
+              placeholder="Select spoken language"
+              onValueChange={(language) =>
+                setFormData({ ...formData, language })
+              }
+            >
+              <Select.Item label="Catalan" value="Catalan" />
+              <Select.Item label="Spanish" value="Spanish" />
+              <Select.Item label="English" value="English" />
+            </Select>
+          </VStack>
+          <VStack>
+            <FormControl.Label>Duration</FormControl.Label>
+
+            <InputGroup
+              w={{
+                base: '45%',
+                md: '285',
+              }}
+            >
+              <Input
+                textAlign={'right'}
+                w={{
+                  base: '70%',
+                  md: '100%',
+                }}
+                onChangeText={(duration: string) =>
+                  setFormData({ ...formData, duration: +duration })
+                }
+              />
+              <InputRightAddon>minutes</InputRightAddon>
+            </InputGroup>
+          </VStack>
+        </HStack>
+      </FormControl>
+
+      <TextArea
+        h={20}
+        placeholder="Event descriptions"
+        w="75%"
+        onChangeText={(description) => {
+          setFormData({ ...formData, description });
+        }}
+        mb="2"
+      />
+
+      <Button
+        size={'md'}
+        onPress={handleSubmit}
+        // isLoading
+        spinnerPlacement="end"
+        isLoadingText="Submitting"
+      >
+        Submit
+      </Button>
     </Center>
   );
 }
