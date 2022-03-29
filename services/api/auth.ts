eb5a47b4283e24f74540bc367e9602c0c8dec900
Adrià M. Cassorla <adria@adriamartinez.cat>
2022-03-29 18:12:50 +0200

    auth/dashboard route from root

--- services/api/auth.ts
+++ services/api/auth.ts
@@ -1,12 +1,11 @@
 import { gql, GraphQLClient } from 'graphql-request';
 import { PromoterProfile } from '../../utils/Types/userTypes';
 
-const apiURL = 'http://localhost:4004/graphql';
+const apiURL = 'https://tourn.me/usher/api';
 const client = new GraphQLClient(apiURL);
 
 export const getPromoterProfile = async () => {
   const token = localStorage.getItem('promoter');
-  console.log('getting promoter')
   if (!token) return null;
   client.setHeader('authorization', `Bearer ${token}`);
 
@@ -73,7 +72,6 @@ export const getPromoterProfile = async () => {
 
   try {
     const { getPromoter } = await client.request(query);
-    console.log(getPromoter);
     if (getPromoter.error) return getPromoter.error
     return getPromoter.promoter
   } catch (error) {
@@ -132,7 +130,6 @@ export const getJWT = async (
 };
 
 export const createPromoter = async (email: string, password: string, name: string, telephone: number) => {
-  console.log('lets create it', email, password, name, telephone)
   const mutation = gql`
     mutation createPromoter($email: String! $password: String! $name: String! $telephone: Int!) {
       createPromoter(email: $email password: $password name: $name telephone: $telephone) {
@@ -155,7 +152,6 @@ export const createPromoter = async (email: string, password: string, name: stri
     const { createPromoter } = await client.request(mutation, { email, password, name, telephone });
     if (createPromoter.error) return createPromoter.error as string
     localStorage.setItem('promoter', createPromoter.token);
-    console.log(createPromoter);
     return createPromoter.promoter as PromoterProfile;
   } catch (e) {
     return 'Network error while creating a new user';
